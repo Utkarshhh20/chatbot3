ef9a5968433d068457d21d0a2f593148db80005b
Utkarshhh20 <88526325+Utkarshhh20@users.noreply.github.com>
2022-08-28 14:55:13 +0400

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -6,8 +6,9 @@ bot=ChatBot('Robo')
 trainer=ChatterBotCorpusTrainer(bot)
 trainer.train('chatterbot.corpus.english')
 def get_bot_response():
-    userText=request.args.get('msg')
-    return str(bot.get_response(userText))
-user_input=st.write('Please enter your message')
-bot_resp=get_bot_response()
-st.write(bot_resp)
+    userText=st.text_input('Please enter your message')
+    if userText!='':
+        return str(bot.get_response(userText))
+while:
+    bot_resp=get_bot_response()
+    st.write(bot_resp)
